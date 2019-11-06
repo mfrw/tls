036b820ed9abc727225d62eba6bc24f28e7929f0
Muhammad Falak R Wani <falakreyaz@gmail.com>
2019-11-06 14:23:43 +0530

    base64: add base64_decode fn

---
+++ include/base64.h
@@ -0,0 +1,8 @@
+#ifndef BASE64_H
+#define BASE64_H
+
+void base64_encode(const unsigned char *, int, unsigned char *);
+int base64_decode(const unsigned char *, int, unsigned char *);
+
+#endif /* end of include guard: BASE64_H */
+
--- src/base64.c
+++ src/base64.c
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <stdio.h>
+
 static char *base64 =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234456789+/";
 
@@ -23,4 +26,47 @@ void base64_encode(const unsigned char *input, int len, unsigned char *output) {
 		input += 3;
 	} while (len -= 3);
 	*output = '\0';
+}
+
+static int unbase64[] = {
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, -1, -1, -1, 0,	-1, -1, -1, 0,	1,  2,	3,  4,	5,  6,	7,  8,	9,  10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1,
+    -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+    43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1, -1
+
+};
+
+int base64_decode(const unsigned char *input, int len, unsigned char *output) {
+	int out_len = 0, i;
+	assert(!(len & 0x03));
+	do {
+		for (i = 0; i <= 3; i++) {
+			if (input[i] > 128 || unbase64[input[i]] == -1) {
+				fprintf(stderr,
+					"Invalid character for base64 "
+					"encoding: %c\n",
+					input[i]);
+				return -1;
+			}
+		}
+		*output++ =
+		    unbase64[input[0]] << 2 | (unbase64[input[1]] & 0x30) >> 4;
+		out_len++;
+
+		if (input[2] != '=') {
+			*output++ = (unbase64[input[1]] & 0x0f) << 4 |
+				    (unbase64[input[2]] & 0x3c) >> 2;
+			out_len++;
+		}
+		if (input[3] != '=') {
+			*output++ = (unbase64[input[2]] & 0x03) << 6 |
+				    unbase64[input[3]];
+			out_len++;
+		}
+		input += 4;
+	} while (len -= 4);
+	return out_len;
 }
